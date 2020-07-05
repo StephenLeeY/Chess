044b7694e67500ec6d377264699a52138e9e848d
StephenLeeY <stephensl0118@gmail.com>
2020-07-05 06:33:13 -0400

    Almost all of the piece logic is working.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-// TODO: Make king unable to move to spaces where it can be captured.
+/*
+  TODO
+  1. FIX: kingMoveCheck not detecting illegal moves for non-pawn pieces
+  2. IMPLEMENT: Checkmate stuff
+  3. IMPLEMENT: Highlight tile with red when invalid move
+  4. IMEPLEMENT: Highlight king with red when in check
+  5. IMPLEMENT: Displays for turn number and move history
+  6. IMEPLEMENT: Basic random computer AI
+  7. IMPLEMENT: Computer AI w/ min-max alpha-beta.
+*/
 
 var canvas = document.getElementById('myCanvas')
 var ctx = canvas.getContext('2d')
@@ -133,6 +142,204 @@ function drawBoard(legalMoves) {
   }
 }
 
+// Removes illegal moves from king's legal moves
+function kingMoveCheck(legalMoves, prevCoor) {
+  let kingLegalMoves = legalMoves
+  let enemyColor = "", enemyPawn = "", enemyKing = ""
+  // Check king color
+  if(pieceMap.get(prevCoor).src.includes("White")) {
+    enemyColor = "Black", enemyPawn = "Black_Pawn", enemyKing = "Black_King"
+  } else {
+    enemyColor = "White", enemyPawn = "White_Pawn", enemyKing = "White_King"
+  }
+
+  // Loop through all pieces on board
+  for(const [coordinate, piece] of pieceMap.entries()) {
+    if(piece.src.includes(enemyColor)) {
+      // Filter out illegal moves caused by enemy pawns
+      if(piece.src.includes(enemyPawn)) {
+        kingLegalMoves = kingLegalMoves.filter(function(ele) {
+          if(((parseInt(coordinate[0]) + 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) + 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) + 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) - 1) == ele[1])) {
+               return
+          }
+          return ele
+        })
+      }
+      // Filter out illegal moves caused by enemy king
+      else if(piece.src.includes(enemyKing)) {
+        kingLegalMoves = kingLegalMoves.filter(function(ele) {
+          if(((parseInt(coordinate[0]) - 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) - 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) - 1) == ele[0]) &&
+             ((parseInt(coordinate[1])) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) - 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) + 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0])) == ele[0]) &&
+             ((parseInt(coordinate[1]) - 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0])) == ele[0]) &&
+             ((parseInt(coordinate[1]) + 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) + 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) - 1) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) + 1) == ele[0]) &&
+             ((parseInt(coordinate[1])) == ele[1])) {
+               return
+          } else if(((parseInt(coordinate[0]) + 1) == ele[0]) &&
+             ((parseInt(coordinate[1]) + 1) == ele[1])) {
+               return
+          }
+          return ele
+        })
+      }
+      // Filter out illegal moves caused by everything else
+      let legalCheck = getLegalMoves(coordinate)
+      kingLegalMoves = kingLegalMoves.filter(ele => !legalCheck.includes(ele))
+    }
+  }
+  return kingLegalMoves
+}
+
+// Handles bishop piece logic
+function bishopHandler(prevCoor, legalMoves, enemyColor) {
+  downLeft:
+  for(let y = parseInt(prevCoor[0]) + 1, x = parseInt(prevCoor[1]) - 1; y < 8 && x >= 0; y++) {
+    if(!pieceMap.has(y + x.toString())) {
+      legalMoves.push(y + x.toString())
+    } else {
+      if(pieceMap.get(y + x.toString()).src.includes(enemyColor)) {
+        legalMoves.push(y + x.toString())
+      }
+      break downLeft
+    }
+    x--
+  }
+  upRight:
+  for(let y = parseInt(prevCoor[0]) - 1, x = parseInt(prevCoor[1]) + 1; y >= 0 && x < 8; y--) {
+    if(!pieceMap.has(y + x.toString())) {
+      legalMoves.push(y + x.toString())
+    } else {
+      if(pieceMap.get(y + x.toString()).src.includes(enemyColor)) {
+        legalMoves.push(y + x.toString())
+      }
+      break upRight
+    }
+    x++
+  }
+  downRight:
+  for(let y = parseInt(prevCoor[0]) + 1, x = parseInt(prevCoor[1]) + 1; y < 8 && x < 8; y++) {
+    if(!pieceMap.has(y + x.toString())) {
+      legalMoves.push(y + x.toString())
+    } else {
+      if(pieceMap.get(y + x.toString()).src.includes(enemyColor)) {
+        legalMoves.push(y + x.toString())
+      }
+      break downRight
+    }
+    x++
+  }
+  upLeft:
+  for(let y = parseInt(prevCoor[0]) - 1, x = parseInt(prevCoor[1]) - 1; y >= 0 && x >= 0; y--) {
+    if(!pieceMap.has(y + x.toString())) {
+      legalMoves.push(y + x.toString())
+    } else {
+      if(pieceMap.get(y + x.toString()).src.includes(enemyColor)) {
+        legalMoves.push(y + x.toString())
+      }
+      break upLeft
+    }
+    x--
+  }
+  return legalMoves
+}
+
+// Handles knight piece logic
+function knightHandler(prevCoor, legalMoves, enemyColor) {
+  // List of possible knight moves
+  let knightMoves = [
+    parseInt(prevCoor[0]) - 2 + (parseInt(prevCoor[1]) + 1).toString(),
+    parseInt(prevCoor[0]) - 1 + (parseInt(prevCoor[1]) + 2).toString(),
+    parseInt(prevCoor[0]) + 1 + (parseInt(prevCoor[1]) + 2).toString(),
+    parseInt(prevCoor[0]) + 2 + (parseInt(prevCoor[1]) + 1).toString(),
+    parseInt(prevCoor[0]) + 2 + (parseInt(prevCoor[1]) - 1).toString(),
+    parseInt(prevCoor[0]) + 1 + (parseInt(prevCoor[1]) - 2).toString(),
+    parseInt(prevCoor[0]) - 1 + (parseInt(prevCoor[1]) - 2).toString(),
+    parseInt(prevCoor[0]) - 2 + (parseInt(prevCoor[1]) - 1).toString()
+  ]
+  // Filter out out of bounds moves
+  knightMoves = knightMoves.filter(function(ele) {
+    if(ele[0] > 7 || ele[0] < 0 || ele[1] > 7 || ele[1] < 0) {
+      return
+    } else {
+      return ele
+    }
+  })
+
+  for(move of knightMoves) {
+    if(!pieceMap.has(move) || pieceMap.get(move).src.includes(enemyColor)) {
+      legalMoves.push(move)
+    }
+  }
+  return legalMoves
+}
+
+// Handles rook piece logic
+function rookHandler(prevCoor, legalMoves, enemyColor) {
+  down:
+  for(let y = parseInt(prevCoor[0]) + 1; y < 8; y++) {
+    if(!pieceMap.has(y + prevCoor[1])) {
+      legalMoves.push(y + prevCoor[1])
+    } else {
+      if(pieceMap.get(y + prevCoor[1]).src.includes(enemyColor)) {
+        legalMoves.push(y + prevCoor[1])
+      }
+      break down
+    }
+  }
+  up:
+  for(let y = prevCoor[0] - 1; y >= 0; y--) {
+    if(!pieceMap.has(y + prevCoor[1])) {
+      legalMoves.push(y + prevCoor[1])
+    } else {
+      if(pieceMap.get(y + prevCoor[1]).src.includes(enemyColor)) {
+        legalMoves.push(y + prevCoor[1])
+      }
+      break up
+    }
+  }
+  right:
+  for(let x = parseInt(prevCoor[1]) + 1; x < 8; x++) {
+    if(!pieceMap.has(prevCoor[0] + x)) {
+      legalMoves.push(prevCoor[0] + x)
+    } else {
+      if(pieceMap.get(prevCoor[0] + x).src.includes(enemyColor)) {
+        legalMoves.push(prevCoor[0] + x)
+      }
+      break right
+    }
+  }
+  left:
+  for(let x = prevCoor[1] - 1; x >= 0; x--) {
+    if(!pieceMap.has(prevCoor[0] + x)) {
+      legalMoves.push(prevCoor[0] + x)
+    } else {
+      if(pieceMap.get(prevCoor[0] + x).src.includes(enemyColor)) {
+        legalMoves.push(prevCoor[0] + x)
+      }
+      break left
+    }
+  }
+  return legalMoves
+}
+
 // Returns legal moves given a coordinate of a piece
 function getLegalMoves(prevCoor) {
   let currPiece = pieceMap.get(prevCoor)
@@ -147,7 +354,7 @@ function getLegalMoves(prevCoor) {
     return legalMoves
   }
 
-  // Legal move checking for pieces
+  // Piece Handling
   if(currPiece.src.includes("White_Pawn")) {
     if(turnNum == 1) {
       legalMoves.push((prevCoor[0] - 2) + prevCoor[1])
@@ -188,83 +395,61 @@ function getLegalMoves(prevCoor) {
         legalMoves.push("C71")
       }
     }
+    // Top Left
     if(!pieceMap.has((prevCoor[0] - 1).toString() + (prevCoor[1] - 1)) ||
         pieceMap.get((prevCoor[0] - 1).toString() + (prevCoor[1] - 1)).src.includes("Black")) {
       legalMoves.push((prevCoor[0] - 1).toString() + (prevCoor[1] - 1))
     }
+    // Top
     if(!pieceMap.has((prevCoor[0] - 1) + prevCoor[1]) ||
         (pieceMap.get((prevCoor[0] - 1) + prevCoor[1]).src.includes("Black"))) {
       legalMoves.push((prevCoor[0] - 1) + prevCoor[1])
     }
+    // Top Right
     if(!pieceMap.has((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1)) ||
         pieceMap.get((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1)).src.includes("Black")) {
       legalMoves.push((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1))
     }
+    // Left
     if(!pieceMap.has(prevCoor[0] + (prevCoor[1] - 1)) ||
         pieceMap.get(prevCoor[0] + (prevCoor[1] - 1)).src.includes("Black")) {
       legalMoves.push(prevCoor[0] + (prevCoor[1] - 1))
     }
+    // Right
     if(!pieceMap.has(prevCoor[0] + (parseInt(prevCoor[1]) + 1)) ||
         pieceMap.get(prevCoor[0] + (parseInt(prevCoor[1]) + 1)).src.includes("Black")) {
       legalMoves.push(prevCoor[0] + (parseInt(prevCoor[1]) + 1))
     }
-    if(!pieceMap.has((parseInt(prevCoor[0]) - 1).toString() + (prevCoor[1] - 1)) ||
-        pieceMap.get((parseInt(prevCoor[0]) - 1).toString() + (prevCoor[1] - 1)).src.includes("Black")) {
-      legalMoves.push((parseInt(prevCoor[0]) - 1).toString() + (prevCoor[1] - 1))
+    // Bottom Left
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1)) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1)).src.includes("Black")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1))
     }
-    if(!pieceMap.has((parseInt(prevCoor[0]) - 1) + prevCoor[1]) ||
-        pieceMap.get((parseInt(prevCoor[0]) - 1) + prevCoor[1]).src.includes("Black")) {
-      legalMoves.push((parseInt(prevCoor[0]) - 1) + prevCoor[1])
+    // Bottom
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1) + prevCoor[1]) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1) + prevCoor[1]).src.includes("Black")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1) + prevCoor[1])
     }
-    if(!pieceMap.has((parseInt(prevCoor[0]) - 1).toString() + (parseInt(prevCoor[1]) + 1)) ||
-        pieceMap.get((parseInt(prevCoor[0]) - 1).toString() + (parseInt(prevCoor[1]) + 1)).src.includes("Black")) {
-      legalMoves.push((parseInt(prevCoor[0]) - 1).toString() + (parseInt(prevCoor[1]) + 1))
+    // Bottom Right
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1)) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1)).src.includes("Black")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1))
     }
+    // Remove all out of index legal moves
+    legalMoves = legalMoves.filter(move => (move[0] <= 7 &&
+      move[0] >= 0 && move[1] <= 7 && move[1] >= 0))
+    // Remove all illegla moves
+    legalMoves = kingMoveCheck(legalMoves, prevCoor)
     return legalMoves
   } else if (currPiece.src.includes("White_Queen")) {
-
+    return bishopHandler(prevCoor, legalMoves, "Black")
+    .concat(rookHandler(prevCoor, legalMoves, "Black"))
   } else if (currPiece.src.includes("White_Bishop")) {
-
+    return bishopHandler(prevCoor, legalMoves, "Black")
   } else if (currPiece.src.includes("White_Knight")) {
-
+    return knightHandler(prevCoor, legalMoves, "Black")
   } else if (currPiece.src.includes("White_Rook")) {
-    down:
-    for(let y = parseInt(prevCoor[0]) + 1; y < 8; y++) {
-      if(!pieceMap.has(y + prevCoor[1])) {
-        legalMoves.push(y + prevCoor[1])
-      } else {
-        legalMoves.push(y + prevCoor[1])
-        break down
-      }
-    }
-    up:
-    for(let y = prevCoor[0] - 1; y >= 0; y--) {
-      if(!pieceMap.has(y + prevCoor[1])) {
-        legalMoves.push(y + prevCoor[1])
-      } else {
-        legalMoves.push(y + prevCoor[1])
-        break up
-      }
-    }
-    right:
-    for(let x = parseInt(prevCoor[1]) + 1; x < 8; x++) {
-      if(!pieceMap.has(prevCoor[0] + x)) {
-        legalMoves.push(prevCoor[0] + x)
-      } else {
-        legalMoves.push(prevCoor[0] + x)
-        break right
-      }
-    }
-    left:
-    for(let x = prevCoor[1] - 1; x >= 0; x--) {
-      if(!pieceMap.has(prevCoor[0] + x)) {
-        legalMoves.push(prevCoor[0] + x)
-      } else {
-        legalMoves.push(prevCoor[0] + x)
-        break left
-      }
-    }
-    return legalMoves
+    return rookHandler(prevCoor, legalMoves, "Black")
   } else if (currPiece.src.includes("Black_Pawn")) {
     if(turnNum == 2) {
       legalMoves.push((parseInt(prevCoor[0]) + 2) + prevCoor[1])
@@ -282,15 +467,84 @@ function getLegalMoves(prevCoor) {
     }
     return legalMoves
   } else if (currPiece.src.includes("Black_King")) {
-
+    // Castling. Legal move for castling has prefix 'C'.
+    castle:
+    if(!moveHistory.has("Black_King")) {
+      let rightCastleAvail = true
+      let leftCastleAvail = true
+      for(rookMoves in moveHistory.get("Black_Rook")) {
+        if(rookMoves.split(",")[0] == "07") {
+          rightCastleAvail = false
+        }
+        if(rookMoves.split(",")[0] == "00") {
+          leftCastleAvail = false
+        }
+        if(!rightCastleAvail && !leftCastleAvail) {
+          break castle
+        }
+      }
+      if(rightCastleAvail && !pieceMap.has("06") && !pieceMap.has("05")) {
+        legalMoves.push("C06")
+      }
+      if(leftCastleAvail && !pieceMap.has("01") && !pieceMap.has("02") && !pieceMap.has("03")) {
+        legalMoves.push("C01")
+      }
+    }
+    // Top Left
+    if(!pieceMap.has((prevCoor[0] - 1).toString() + (prevCoor[1] - 1)) ||
+        pieceMap.get((prevCoor[0] - 1).toString() + (prevCoor[1] - 1)).src.includes("White")) {
+      legalMoves.push((prevCoor[0] - 1).toString() + (prevCoor[1] - 1))
+    }
+    // Top
+    if(!pieceMap.has((prevCoor[0] - 1) + prevCoor[1]) ||
+        (pieceMap.get((prevCoor[0] - 1) + prevCoor[1]).src.includes("White"))) {
+      legalMoves.push((prevCoor[0] - 1) + prevCoor[1])
+    }
+    // Top Right
+    if(!pieceMap.has((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1)) ||
+        pieceMap.get((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1)).src.includes("White")) {
+      legalMoves.push((prevCoor[0] - 1).toString() + (parseInt(prevCoor[1]) + 1))
+    }
+    // Left
+    if(!pieceMap.has(prevCoor[0] + (prevCoor[1] - 1)) ||
+        pieceMap.get(prevCoor[0] + (prevCoor[1] - 1)).src.includes("White")) {
+      legalMoves.push(prevCoor[0] + (prevCoor[1] - 1))
+    }
+    // Right
+    if(!pieceMap.has(prevCoor[0] + (parseInt(prevCoor[1]) + 1)) ||
+        pieceMap.get(prevCoor[0] + (parseInt(prevCoor[1]) + 1)).src.includes("White")) {
+      legalMoves.push(prevCoor[0] + (parseInt(prevCoor[1]) + 1))
+    }
+    // Bottom Left
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1)) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1)).src.includes("White")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1).toString() + (prevCoor[1] - 1))
+    }
+    // Bottom
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1) + prevCoor[1]) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1) + prevCoor[1]).src.includes("White")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1) + prevCoor[1])
+    }
+    // Bottom Right
+    if(!pieceMap.has((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1)) ||
+        pieceMap.get((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1)).src.includes("White")) {
+      legalMoves.push((parseInt(prevCoor[0]) + 1).toString() + (parseInt(prevCoor[1]) + 1))
+    }
+    // Remove all out of index legal moves
+    legalMoves = legalMoves.filter(move => (move[0] <= 7 &&
+      move[0] >= 0 && move[1] <= 7 && move[1] >= 0))
+    // Remove all illegla moves
+    legalMoves = kingMoveCheck(legalMoves, prevCoor)
+    return legalMoves
   } else if (currPiece.src.includes("Black_Queen")) {
-
+    return bishopHandler(prevCoor, legalMoves, "White")
+    .concat(rookHandler(prevCoor, legalMoves, "White"))
   } else if (currPiece.src.includes("Black_Bishop")) {
-
+    return bishopHandler(prevCoor, legalMoves, "White")
   } else if (currPiece.src.includes("Black_Knight")) {
-
+    return knightHandler(prevCoor, legalMoves, "White")
   } else if (currPiece.src.includes("Black_Rook")) {
-
+    return rookHandler(prevCoor, legalMoves, "White")
   } else {
     return legalMoves
   }
@@ -325,9 +579,15 @@ function main() {
           ctx.clearRect(0, 0, canvas.width, canvas.height)
           drawBoard([])
           turnNum += 1
+          secondClick = false
+          prevCoor = ""
+        } else {
+          // Consider highlighting selected piece in red
+          secondClick = false
+          prevCoor = ""
+          ctx.clearRect(0, 0, canvas.width, canvas.height)
+          drawBoard([])
         }
-        secondClick = false
-        prevCoor = ""
       } else {
         secondClick = true
         prevCoor = currCoor
