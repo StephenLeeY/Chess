f202ec818517dffb8f1e794db6afdf90fdc86339
StephenLeeY <stephensl0118@gmail.com>
2020-12-13 05:47:19 -0500

    Highlight & Moving functionality

--- js/chess.js
+++ js/chess.js
@@ -1,4 +1,5 @@
-// Chess logic handlers
+// TODO:
+// 1. Figure out dragging mechanics for pieces
 
 // locations of all pieces, player to move, history of moves
 class State {
@@ -31,6 +32,10 @@ class State {
     this.board.map((row) => row.reverse());
     this.board = this.board.reverse();
   }
+
+  move_piece(coor1, coor2) {
+    [this.board[coor1[0]][coor1[1]], this.board[coor2[0]][coor2[1]]] = [this.board[coor2[0]][coor2[1]], this.board[coor1[0]][coor1[1]]];
+  }
 }
 
 // type of piece, color, number of points, etc.
@@ -42,13 +47,17 @@ class Piece {
 }
 
 class UIHandler {
-  constructor(state, parent_div, board_dims, dark_color, light_color) {
+  constructor(state, board_dims, dark_color, light_color) {
+    this.board_div = document.getElementById('board');
     this.state = state;
-    this.parent_div = parent_div;
     this.board_dims = board_dims;
     this.dark_color = dark_color;
     this.light_color = light_color;
     this.square_size = this.calc_square_size();
+    this.highlighted_square = [null, ""];
+
+    this.draw_board();
+    this.draw_pieces();
   }
 
   calc_square_size() {
@@ -56,52 +65,88 @@ class UIHandler {
     boundary *= 0.8;
     let largest_dim = (this.board_dims[0] > this.board_dims[1]) ? this.board_dims[0] : this.board_dims[1];
 
-    this.parent_div.style.width = boundary + "px";
-    this.parent_div.style.height = boundary + "px";
+    this.board_div.style.width = boundary + "px";
+    this.board_div.style.height = boundary + "px";
 
     return boundary / largest_dim;
   }
 
   draw_board() {
-    for(let x = 0; x < this.board_dims[0]; x++) {
+    for(let y = 0; y < this.board_dims[0]; y++) {
       const row = document.createElement("div");
       row.className = "chess-row";
 
-      for(let y = 0; y < this.board_dims[1]; y++) {
+      for(let x = 0; x < this.board_dims[1]; x++) {
         const square = document.createElement("div");
         square.className = ((x + y) % 2 === 0) ? "lightSquare" : "darkSquare";
+        square.id = y + "," + x;
         square.style.width = this.square_size + "px";
         square.style.height = this.square_size + "px";
 
+        let _this = this;
+        square.addEventListener('click',
+          function() {
+            if(_this.highlighted_square[0] !== null) {
+              // Move piece
+              const coor1 = _this.highlighted_square[0].id.split(",");
+              const coor2 = this.id.split(",");
+              _this.state.move_piece(coor1, coor2);
+              _this.draw_pieces();
+
+              // Unhighlight square
+              _this.highlighted_square[0].className = _this.highlighted_square[1];
+
+              // Reset
+              _this.highlighted_square = [null, ""];
+            } else if(this.children.length > 0) {
+              // Store highlighted square information
+              _this.highlighted_square = [this, this.className];
+              this.className = "highlightedSquare";
+            }
+          }, false);
+
         row.appendChild(square);
       }
-      this.parent_div.appendChild(row);
+      this.board_div.appendChild(row);
+    }
+  }
+
+  clear_board() {
+    for(let x = 0; x < this.board_dims[0]; x++) {
+      for(let y = 0; y < this.board_dims[1]; y++) {
+        const square = this.board_div.children[x].children[y];
+        if(square.lastChild !== null) {
+          square.removeChild(square.lastChild);
+        }
+      }
     }
   }
 
   draw_pieces() {
-    for(let x = 0; x < this.board_dims[0]; x++) {
-      for(let y = 0; y < this.board_dims[1]; y++) {
+    this.clear_board();
+    for(let y = 0; y < this.board_dims[0]; y++) {
+      for(let x = 0; x < this.board_dims[1]; x++) {
         const piece = document.createElement("img");
         piece.className = "chess-piece";
         piece.style.width = this.square_size + "px";
         piece.style.height = this.square_size + "px";
 
-        const piece_object = this.state.board[x][y];
+        const piece_object = this.state.board[y][x];
         if(piece_object.type === "Empty" || piece_object.color === "None") {
           continue;
         }
         piece.src = "Resources/Pieces/" + piece_object.color + "_" + piece_object.type + ".png";
 
-        this.parent_div.children[x].children[y].appendChild(piece);
+        this.board_div.children[y].children[x].appendChild(piece);
       }
     }
   }
 }
 
-function main(board_div) {
-  let init_state = new State();
-  let drawer = new UIHandler(init_state, document.getElementById("board"), [8, 8], "#8b6914", "#deb887");
-  drawer.draw_board();
-  drawer.draw_pieces();
+function main() {
+  let state = new State();
+  let drawer = new UIHandler(state, [8, 8], "#8b6914", "#deb887");
+
+  // Switch Sides handler
+  document.getElementById('switch').addEventListener('click', function() { state.flip_board(); drawer.draw_pieces(); }, false);
 }
